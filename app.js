0f7cf2b492d63e666cc7a614a9dee09ea1660971
jaguar6105 <gilljoseph603@gmail.com>
2020-10-27 17:21:10 -0400

    fix comments

--- app.js
+++ app.js
@@ -10,31 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
 //asks the user questions and create the object from the questions
 async function askQuestions(role) {
     try {
@@ -80,6 +55,8 @@ async function askQuestions(role) {
     }
 }
 
+
+//the program running function
 async function init() {
     const employeeList = [];
     let manager = await askQuestions("Manager");
@@ -133,6 +110,7 @@ async function askUserRole() {
     }
 }
 
+//writes text to team.htlm in the output folder
 function writeToFile(data) {
     fs.writeFileSync("./output/team.html", data);
 }
